dca75870777e34faec8b94317a1d3098d43ffcb1
michaelssss <liangyuming2@gmail.com>
2017-02-19 22:46:56 +0800

    Finish Stack

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,15 @@
 package com.company;
 
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-        List<String> list = new MyArrayList<>(2);
-        list.add("A");
-        list.add("B");
-        list.add("C");
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+        Stack stack = new Stack(2);
+        stack.push("Haha");
+        stack.push("heiheihei");
+        stack.push("lalalalal");
+        System.out.println(stack.peek("Haha"));
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
         }
     }
 }
